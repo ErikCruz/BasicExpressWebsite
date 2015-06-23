479e3e7e7e28f81edf0dbddbb0e26b84dd6fef31
Erik Cruz <erikc1989@live.com>
2015-06-23 23:06:28 +0000

    Implement sending email using nodemailer

--- routes/contact.js
+++ routes/contact.js
@@ -1,8 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+var GmailCredentials = require('../config');
 
 router.get('/', function(req, res, next){
     res.render('contact', {title: 'Contact'});
+});
+
+router.post('/send', function(req, res, next){
+   var transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+          user: GmailCredentials.user,
+          pass: GmailCredentials.pass
+      }
+   });
+   var mailOptions = {
+       from: req.body.email,
+       to: GmailCredentials.to,
+       subject: 'Website Submission',
+       text: 'You have a new submission!',
+       html: '<p>You got a new submission with the following details...</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+   };
+   
+   transporter.sendMail(mailOptions, function(err, info){
+       if(err) {
+           console.log(err);
+           res.redirect('/');
+       }
+       else {
+           console.log('Messege Sent: ' + info.response);
+           res.redirect('/');
+       }
+   });
 });
 
 module.exports = router;
